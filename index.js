6731a8d1607eabf90330a8bee57f0b11a622cf41
Rodrigo Lima <rodrigoscarpimlima@hotmail.com>
2019-06-19 22:33:47 -0300

    prod env 3

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const port = process.env.PORT || 1337;
 // npm run dev
 // npm run test
 
-const carro = {
-    name: "Ferrari",
-    year: 2019
-}
-
-const output = JSON.stringify(carro)
-console.log(output)
-
 app.listen(port, () => {
 	console.log('Server is running in port '+port);
 });
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+const connect = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/task-api-test";
+
+mongoose.connect(connect, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
